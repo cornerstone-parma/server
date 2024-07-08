8cb5fe02a21d13bf49fade4c0d16cd4339b5ca2e
Mykyta <mp091689@gmail.com>
2024-07-08 16:53:55 -0400

    add gitignore

--- CornertoneManager/obj/Debug/net8.0/CornertoneManager.AssemblyInfo.cs
+++ CornertoneManager/obj/Debug/net8.0/CornertoneManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CornertoneManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e9ec4d5527085ade8673604d2413f79e74c1e85")]
 [assembly: System.Reflection.AssemblyProductAttribute("CornertoneManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CornertoneManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
